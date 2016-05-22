121bb55030f2731be1a7bde4bafd6eb99b2078f7
Rodrigo Maller <rmallermartins@gmail.com>
2016-05-22 16:09:20 -0300

    Internationalization and localization

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
+from datetime import date
 from selenium import webdriver
+from django.utils import formats
 from django.core.urlresolvers import reverse
+from django.utils.translation import activate
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 class HomeNewVisitorTest(StaticLiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
+        activate('en')
 
     def tearDown(self):
         self.browser.quit()
@@ -30,3 +35,22 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn("Not Found", self.browser.title)
         self.browser.get(self.live_server_url + "/humans.txt")
         self.assertNotIn("Not Found", self.browser.title)
+
+    def test_localization(self):
+        today = date.today()
+        for lang in ['en', 'pt-br']:
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            local_date = self.browser.find_element_by_id("local-date")
+            non_local_date = self.browser.find_element_by_id("non-local-date")
+            self.assertEqual(formats.date_format(today, use_l10n=True),
+                             local_date.text)
+            self.assertEqual(today.strftime('%Y-%m-%d'), non_local_date.text)
+
+    def test_time_zone(self):
+        self.browser.get(self.get_full_url("home"))
+        tz = self.browser.find_element_by_id("time-tz").text
+        utc = self.browser.find_element_by_id("time-utc").text
+        ny = self.browser.find_element_by_id("time-ny").text
+        self.assertNotEqual(tz, utc)
+        self.assertNotIn(ny, [tz, utc])
--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext_lazy as _
 
 
 def get_env_variable(var_name):
@@ -49,6 +50,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
@@ -65,6 +67,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.i18n',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
@@ -114,9 +117,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LANGUAGES = (
+    ('en', _('English')),
+    ('pt-br', _('Portuguese (Brazil)')),
+)
+
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
